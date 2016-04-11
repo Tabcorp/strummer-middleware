6b424a4bd9552aac707749c2aa96cf287e4c22d5
nicholasf <nicholas.faiz@gmail.com>
2016-04-11 13:32:11 +1000

    Report groups of errors accurately.

--- index.js
+++ index.js
@@ -8,21 +8,34 @@ var AREAS = {
 };
 
 module.exports = function(checks) {
-
     // first validate the middleware options
     validateOptions(checks);
 
     // return actual middleware
     // unwind loop for better performance
     return function(req, res, next) {
-        var err = errorsFrom(checks, req, 'params')
-               || errorsFrom(checks, req, 'query')
-               || errorsFrom(checks, req, 'body')
-               || errorsFrom(checks, req, 'headers')
-               || null;
-        next(err);
-    };
+        var errors = [];
+        var collectedDetails = [];
+        var collectedAreas = [];
 
+        Object.keys(AREAS).forEach(function(area) {
+            var err = errorsFrom(checks, req, area);
+            if (err) {
+                errors.push(err);
+                collectedAreas.push(area);
+                Array.prototype.push.apply(collectedDetails, err.details);
+            }
+        });
+
+        if (errors.length > 1) {
+            var msg = 'There were combined errors in ' + (collectedAreas.join(', ')) + '.';
+            var err = new Error(msg);
+            err.details = collectedDetails;
+            next(err);
+        } else {
+            next(errors[0]);
+        }
+    };
 };
 
 function validateOptions(checks) {
@@ -47,7 +60,7 @@ function isMatcher(check) {
 
 function errorsFrom(checks, req, area) {
     if (checks[area]) {
-        var result = checks[area].match(null, req[area]);
+        var result = checks[area].match(area, req[area]);
         if (result.length > 0) {
             var err = new Error(AREAS[area]);
             err.details = result;
